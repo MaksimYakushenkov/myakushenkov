005c61ca39c778353b3a1e22fe46870aefdbdea6
Maksim Yakushenkov <breakingwebnews@icloud.com>
2023-01-31 18:25:58 +0300

    Feat: Deploy

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -16,14 +16,15 @@ export default function App({ Component, pageProps, router }: AppProps): JSX.Ele
 
   return <>
   <Head>
-    <title>Максим Якушенков | Веб-разработчик</title>
-    <meta name="description" content="Меня зовут Максим, я веб-разработчик с опытом работы 3+ лет. Нахожусь в поисках работы на позицию full stack web-developer / middle web-developer. Уверен, что смогу усилить вашу компанию, ведь мне нравится работать над интересными и значимыми проектами, которыми я могу гордиться, и я всегда стремлюсь создавать чистые, адаптивные интерфейсы, которые плавно анимируются." />
-    <meta name="keywords" content="веб-разработчик, web-developer, resume, cv, web, react, next, резюме, поиск работы" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link rel="preconnect" href="https://mc.yandex.ru"/>
-    <link rel="icon" href="/favicon.ico" />
+    <title>Максим Якушенков | Full Stack Web-Developer</title>
+    <meta name='description' content='Меня зовут Максим, я веб-разработчик с опытом работы 3+ лет. Нахожусь в поисках работы на позицию full stack web-developer / middle web-developer. Уверен, что смогу усилить вашу компанию, ведь мне нравится работать над интересными и значимыми проектами, которыми я могу гордиться, и я всегда стремлюсь создавать чистые, адаптивные интерфейсы, которые плавно анимируются.' />
+    <meta name='keywords' content='веб-разработчик, web-developer, resume, cv, web, react, next, резюме, поиск работы' />
+    <meta name='viewport' content='width=device-width, initial-scale=1' />
+    <link rel='preconnect' href='https://mc.yandex.ru'/>
+    <link rel='icon' href='/favicon.ico' />
     <meta property='og:url' content='https://myakushenkov.ru/' />
     <meta property='og:locale' content='ru_RU' />
+    <meta property='og:image' content='https://myakushenkov.ru/OG_IMAGE.webp' />
   </Head>
   <YMInitializer
     accounts={[]} //id счетиков
